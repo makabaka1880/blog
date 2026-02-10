c39b8c4a0a03cd1dbeb545a579fa58fc01bf0845
makabaka1880 <makabaka1880@outlook.com>
2026-02-11 06:30:05 +0800

    feat: made drink animation 3d

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,12 +1,7 @@
 <template>
     <section class="hero">
         <!-- ART LAYER -->
-        <div class="art">
-            <link rel="preload" href="/static/drink-loop.gif" as="image">
-            <video v-if="!videoEnded" ref="videoElement" src="/static/drink-1.mp4" autoplay muted playsinline
-                draggable="false" @ended="onVideoEnded" />
-            <img v-else ref="videoElementLoop" src="/static/drink-loop.gif" draggable="false" />
-        </div>
+        <DrinkAnimation class="art" />
 
         <!-- CONTENT LAYER -->
         <div class="content">
@@ -141,10 +136,8 @@ watch(videoEnded, (ended) => {
 
     mix-blend-mode: darken;
 
-    video,
-    img {
-        width: 100%;
-        height: auto;
+    :deep(canvas) {
+        pointer-events: auto;
     }
 }
 
