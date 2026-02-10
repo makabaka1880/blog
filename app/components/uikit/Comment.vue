659f160ad3a18b22bf291d6cb04906003475785b
makabaka1880 <makabaka1880@outlook.com>
2026-02-10 19:47:18 +0800

    feat: created comments section

---
+++ app/components/uikit/Comment.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Comments</h2>
+        <div id="tcomment">
+            Comments Loading...
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const appConfig = useAppConfig()
+
+console.log(appConfig.twikooConfig.envID)
+onMounted(() => {
+    window.twikoo?.init({
+        envId: appConfig.twikooConfig.envID,
+        el: "#tcomment"
+    })
+})
+</script>
+
+<style lang="scss" scoped>
+:deep(.twikoo) button {
+    background-color: var(--color-accent)
+}
+</style>
