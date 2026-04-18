10c49702d375244d0140149e3cfbcf7b897ba006
makabaka1880 <makabaka1880@outlook.com>
2026-04-18 13:00:44 +0800

    fix: updated wrong filenames

---
+++ app/components/content/boxes/HintBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <BaseBox :background-char="'\u{1F4A1}'" :box-type="'hint'" :box-class="`hint-box`">
+        <template #header>
+            HINT
+        </template>
+        <slot />
+    </BaseBox>
+</template>
+
+<script lang="ts" setup>
+import BaseBox from './BaseBox.vue'
+
+</script>
+
+<style lang="scss" scoped>
+.hint-box {
+    --box-bg-char-opacity: 0.15;
+    --box-bg-char-filter: saturate(0);
+}
+</style>
